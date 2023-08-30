6ae2191f2071b622e7711ee721ae7f533a2068e3
dattran <dattran@vccorp.vn>
2023-08-30 15:02:00 +0700

    update

--- benchmark/build.gradle.kts
+++ benchmark/build.gradle.kts
@@ -38,6 +38,6 @@ dependencies {
 
 androidComponents {
     beforeVariants(selector().all()) {
-        it.enabled = it.buildType == "benchmark"
+        it.enable = it.buildType == "benchmark"
     }
 }
